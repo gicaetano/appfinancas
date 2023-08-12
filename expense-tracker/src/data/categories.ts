decfa1a0b73d1dbfb64a4c88ac8923234721b74b
Gisele <gisele_sig@hotmail.com>
2023-08-12 15:28:39 -0300

    project initial

---
+++ expense-tracker/src/data/categories.ts
@@ -0,0 +1,7 @@
+import {} from '../types/Category';
+
+export const categories = {
+    food: { title: 'Alimentação', color: 'blue', expense: true },
+    rent: { title: 'Aluguel', color: 'brown', expense: true },
+    salary: { title: 'Salario', color: 'green', expense: false },
+}
