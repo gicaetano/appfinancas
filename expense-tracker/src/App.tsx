081da3d8555113cbf3a3f3d2754c2c9068811346
gisele.caetano <gisele_sig@hotmail.com>
2023-08-13 20:34:11 -0300

    add conf

--- expense-tracker/src/App.tsx
+++ expense-tracker/src/App.tsx
@@ -1,6 +1,14 @@
+import React, { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { categories } from './data/categories';
+import { items } from './data/items';
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState();
+
   return (
     <C.Container>
       <C.Header>
@@ -16,7 +24,8 @@ const App = () => {
 
         </C.Body> 
     </C.Container>
-  );
+ 
+ );
 }
 
 export default App;
