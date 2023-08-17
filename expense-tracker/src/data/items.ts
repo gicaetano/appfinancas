d45873eb57c71916268b5443f69f6198e88ca382
Gisele <gisele_sig@hotmail.com>
2023-08-17 20:42:49 -0300

    finalize part one

--- expense-tracker/src/data/items.ts
+++ expense-tracker/src/data/items.ts
@@ -1,8 +1,8 @@
 import { Item } from '../types/Item';
 
 export const items: Item[] = [
-    { date: new Date(2023, 9, 15), category: 'food', title: 'mcDonalds', value: 32.12 },
-    { date: new Date(2023, 9, 15), category: 'food', title: 'Burguer King', value: 28 },
-    { date: new Date(2023, 9, 16), category: 'rent', title: 'Aluguel', value: 2300 },
-    { date: new Date(2023, 9, 15), category: 'salary', title: 'Salario', value: 4500 },
+    { date: new Date(2023, 7, 6), category: 'food', title: 'mcDonalds', value: 32.12 },
+    { date: new Date(2023, 7, 15), category: 'food', title: 'Burguer King', value: 28 },
+    { date: new Date(2023, 7, 16), category: 'rent', title: 'Aluguel', value: 2300 },
+    { date: new Date(2023,8, 15), category: 'salary', title: 'Salario', value: 4500 },
 ];
